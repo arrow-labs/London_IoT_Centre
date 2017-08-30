0ea8df7e05a360f918edcf2625bb09873f9f6d99
david fearne <david@virtualfront.co.uk>
2017-08-30 16:20:50 +0100

    Readme.md update

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# londoniotcentre
+# Arrow London IoT centre
+
+## Abstract
+Arrow is uniquely positioned in the IoT markets, with a wide portfolio addressing
+every layer of the IoT solution stack, ranging from electronic components to IT
+solutions. Through the launch of the ‘Innovator Program’, Arrow will enable the
+channel community and IoT solution providers to benefit from its IoT technology
+reach and skills. Additionally, other benefits include access to all services delivered by
+Arrow such as remote management and monitoring, support, design, integration,
+training, marketing and financial solutions.
+
+## This Repo
+This repo is designed to openly collaborate with our partners and vendors to create an IoT centre of excellence in Arrow ECS London.
---
+++ SmartHealthCare/Ardiuno/nodeMCU_rfid_reader/nodeMCU_rfid_reader/nodeMCU_rfid_reader.ino
@@ -0,0 +1,108 @@
+
+
+
+/* 
+ *  Code developed and maintained by David Fearne
+ *  wiring Diagram can be found at https://github.com/arrow-labs/london_iot_centre/tree/master/SmartHealthCare/Ardiuno/
+ */
+
+
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266WiFi.h>
+#include <SPI.h>
+#include "MFRC522.h"
+
+
+#define RST_PIN  5  // RST-PIN für RC522 - RFID - SPI - Modul GPIO5 
+#define SS_PIN  4  // SDA-PIN für RC522 - RFID - SPI - Modul GPIO4 
+
+const char *ssid =  "***********";     // change according to your Network - cannot be longer than 32 characters!
+const char *pass =  "***********"; // change according to your Network
+
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
+
+void setup() {
+  Serial.begin(9600);    // Initialize serial communications
+  delay(250);
+  Serial.println(F("Booting...."));
+  
+  SPI.begin();           // Init SPI bus
+  mfrc522.PCD_Init();    // Init MFRC522
+  
+  WiFi.begin(ssid, pass);
+  
+  int retries = 0;
+  while ((WiFi.status() != WL_CONNECTED) && (retries < 10)) {
+    retries++;
+    delay(500);
+    Serial.print(".");
+  }
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println(F("WiFi connected"));
+  }
+  
+  Serial.println(F("Ready!"));
+  Serial.println(F("======================================================")); 
+  Serial.println(F("Scan for Card and print UID:"));
+}
+
+void loop() { 
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    delay(500);
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    delay(500);
+    return;
+  }
+  // Show some details of the PICC (that is: the tag/card)
+  Serial.print(F("Card UID:"));
+  dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
+  Serial.println();
+  //post request
+  if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
+ 
+   HTTPClient http;    //Declare object of class HTTPClient
+ 
+   http.begin("http://192.168.1.88:8085/hello");      //Specify request destination
+   http.addHeader("Content-Type", "text/plain");  //Specify content-type header
+ 
+   int httpCode = http.POST(mfrc522.uid.uidByte, mfrc522.uid.size);   //Send the request
+   String payload = http.getString();                  //Get the response payload
+ 
+   Serial.println(httpCode);   //Print HTTP return code
+   Serial.println(payload);    //Print request response payload
+ 
+   http.end();  //Close connection
+ 
+ }else{
+ 
+    Serial.println("Error in WiFi connection");   
+ 
+ }
+ 
+  delay(30000);  //Send a request every 30 seconds
+
+
+
+
+
+  
+}
+
+// Helper routine to dump a byte array as hex values to Serial
+void dump_byte_array(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
+}
+
+
